e5b258a284ed4b9eaa5910fe30245ffa0a2b97b3
Luiz Felipe <luiz.silva16@estudante.ufla.br>
2021-04-06 18:29:11 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget {
+  MyPage({Key key}) : super(key: key);
+
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  int count = 0;
+
+  void _counter() {
+    setState(() {
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meu Contador'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('$count'),
+      ),
+      floatingActionButton:
+          FloatingActionButton(child: Text('Add'), onPressed: _counter),
+    );
+  }
+}
